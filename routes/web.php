37c4f89c137be8888d7d1549e268b59605426cf7
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-08-23 23:21:47 -0300

    refatore: modificated route fornecedor

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,44 @@ use Illuminate\Support\Facades\Route;
  */
 
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function () {
+    return 'login';
+})->name('site.login');
 
-Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos']);
+/**
+ * possivel fazer o agrupamento de rotas utilizando GROUP
+ * renomeando as rotas utilizando name
+ */
+Route::prefix('/app')->group(function () {
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+    Route::get('/clientes', function () {
+        return 'clientes';
+    })->name('app.clientes');
 
-Route::get('/login', function() { return 'login'; });
-Route::get('/clientes', function() { return 'clientes'; });
-Route::get('/fornecedores', function() { return 'fornecedores'; });
-Route::get('/produtos', function() { return 'produtos'; });
+    Route::get(
+        '/fornecedores',
+        [\App\Http\Controllers\FornecedorController::class, 'index']
+    )->name('app.fornecedores');
 
+    Route::get(
+        '/produtos',
+        function () {
+            return 'produtos';
+        }
+    )->name('app.produtos');
+});
 
+/**
+ * rota para teste envio de parâmetro
+ */
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+
+/**
+ * rota para fallback error 404
+ */
+Route::fallback(function () {
+    echo 'A rota não foi encontrada, para voltar a página menu: <a href="' . route('site.index') . '">clique aqui</a>';
+});
