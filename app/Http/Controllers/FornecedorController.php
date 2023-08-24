7a0c8e67380149f9804c309a545048ac0e610443
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-08-23 23:19:07 -0300

    feat: new controller of fornecedor with connection view

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    //
+    public function index()
+    {
+        $fornecedores = [
+            0 => [
+                'nome' => 'fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000-00'
+            ],
+            1 => [
+                'nome' => 'fornecedor 2',
+                'status' => 'S',
+                'cnpj' => ''
+            ],
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
+       
+    }
 }
