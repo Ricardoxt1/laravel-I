276f1be106de92fa1b933e8afe9adcbfe640cdd2
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-08-24 12:04:54 -0300

    refatore: modificated code implemetation with functions and operations the php

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -12,16 +12,27 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/000-00'
+                'cnpj' => '00.000.000/000-00',
+                'ddd' => '14', //quintana
+                'telefone' => '99840-0340'
             ],
             1 => [
                 'nome' => 'fornecedor 2',
                 'status' => 'S',
-                'cnpj' => ''
+                'cnpj' => '00.000.222/000-00',
+                'ddd' => '11', //são paulo
+                'telefone' => '99783-0340'
+
+            ],
+            2 => [
+                'nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '02.000.000/000-00',
+                'ddd' => '32', //ceara
+                'telefone' => '99320-0340'
             ],
         ];
 
         return view('app.fornecedor.index', compact('fornecedores'));
-       
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,32 +2,20 @@
 
 
 @php
-    /*
-        if(empty($variavel)) {} //retornar true se a variavel estiver vazia
-    */
-
-    /*
-        - ''
-        - 0
-        - 0.0
-        - '0'
-        - null
-        - false
-        - array ()
-        - $var 
-    */
-
+   
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    @isset($fornecedores[1]['cnpj'])
-        CNPJ: {{ $fornecedores[1]['cnpj'] }}
-        @empty($fornecedores['1']['cnpj'])
-        - Vazio
-        @endempty
-    @endisset
+    
+    @foreach ($fornecedores as $indice => $fornecedor )
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'dado não foi informado' }}
+        <br>
+        Telefone: {{ $fornecedor['ddd'] ?? 'dado não foi informado' }} {{ $fornecedor['telefone'] ?? 'dado não foi informado' }}
+        <hr>
+    @endforeach(isset($fornecedores[$i]))
+      
 @endisset
