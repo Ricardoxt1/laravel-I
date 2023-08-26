3b0e10fb87dcfb976ef4dea60ab7f07875898455
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-08-26 17:52:58 -0300

    feat: operating with php condition types

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,21 +1,31 @@
 <h3>fornecedor</h3>
 
-
-@php
-   
-@endphp
-
 @isset($fornecedores)
-    
-    @foreach ($fornecedores as $indice => $fornecedor )
-        Fornecedor: {{ $fornecedor['nome'] }}
+    @forelse ($fornecedores as $indice => $fornecedor )
+    Fornecedor: {{ $fornecedor['nome'] }}
         <br>
         Status: {{ $fornecedor['status'] }}
         <br>
         CNPJ: {{ $fornecedor['cnpj'] ?? 'dado não foi informado' }}
         <br>
         Telefone: {{ $fornecedor['ddd'] ?? 'dado não foi informado' }} {{ $fornecedor['telefone'] ?? 'dado não foi informado' }}
+
+        @if($loop->first)
+            <br>
+            Primeiro índice do loop
+            <br>
+            Contagem índice do loop {{ ($loop->count) }}   
+        @endif
+
+        @if($loop->last)
+            <br>
+            Último índice do loop
+        @endif
         <hr>
-    @endforeach(isset($fornecedores[$i]))
-      
+    
+        
+    
+    @empty
+        Não existe fornecedores cadastrados!!!
+    @endforelse()
 @endisset
